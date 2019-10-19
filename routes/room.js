e2edbf12ca71d5cc7298c506aaf642e938c07433
xxzoing@qq.com <xxzoing@qq.com>
2019-10-20 00:17:24 +0800

    【fix】初始化一个空房间

--- routes/room.js
+++ routes/room.js
@@ -7,8 +7,7 @@ router.post('/list', async (req, res) => {
     if (req.session.name) {
         res.json(json(Object.values(rooms).map(item => item.info)));
     } else {
-        res.json(json({ isLogin: false }));
+        res.json(json({  }));
     }
 });
-
 module.exports = router;
